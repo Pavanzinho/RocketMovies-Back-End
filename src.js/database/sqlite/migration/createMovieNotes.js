976dca0ef6c5a35a8693e3ca8009cb756c27fdcb
pavan <thiago772000@gmail.com>
2023-03-31 18:07:36 -0300

    fixed MovieNotes tab

--- src.js/database/sqlite/migration/createMovieNotes.js
+++ src.js/database/sqlite/migration/createMovieNotes.js
@@ -1,14 +1,18 @@
-const createMovieNotes = `
-    CREATE TABLE IF NOT EXISTS movie_notes (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        movie_title TEXT,
-        movie_description TEXT,
-        rating INTEGER CHECK (rating >= 1 AND rating <= 5),
-        user_id INTEGER REFERENCES users(id),
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-       
-    );
-`;
 
-module.exports=createMovieNotes;
+
+const createMovieNotes = `
+CREATE TABLE IF NOT EXISTS movie_notes (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  movie_title TEXT,
+  movie_description TEXT,
+  rating INTEGER CHECK (rating >= 1 AND rating <= 5),
+  user_id INTEGER REFERENCES users(id),
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+`
+
+module.exports = createMovieNotes;
+
+
+
